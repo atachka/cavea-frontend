e4ac69c75e04cdad9b9830179b3d6bfbe67b9739
atachka <56038697+atachka@users.noreply.github.com>
2023-05-09 16:24:53 +0400

    fixed api bug

--- src/utils/getUrl.ts
+++ src/utils/getUrl.ts
@@ -4,8 +4,8 @@ import { FilterParams } from "../types";
 export const getUrl = (queryParams?: FilterParams) => {
   if (queryParams) {
     const { name, address, page, prices } = queryParams;
-    return `${apiUrl}/inventory?offset=20&name=${name}&address=${address}&page=${page}
+    return `${apiUrl}/inventories?offset=20&name=${name}&address=${address}&page=${page}
     &minPrice=${prices?.minPrice}&maxPrice=${prices?.maxPrice}`;
   }
-  return `${apiUrl}/inventory`;
+  return `${apiUrl}/inventories`;
 };
